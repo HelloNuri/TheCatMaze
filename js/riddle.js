cff06435565c3b9d405674eaf7a8dc7a491ee154
HelloNuri <HelloNuri.dvl@gmail.com>
2018-11-29 15:59:49 +0900

    bootstrap, prevent drag, fixed bug

--- js/riddle.js
+++ js/riddle.js
@@ -1,6 +1,7 @@
   $('.container.hint').click(function() {
 
-    $("html, body").animate({
-      scrollTop: $(document).height()
-    }, 1500);
+    // $("html, body").animate({
+    //   scrollTop: $(document).height()
+    // }, 1500);
+    $("html, body").scrollTop($(document).height())
   });
