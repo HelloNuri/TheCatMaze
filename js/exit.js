1d22dce235bc2d4bb2a6953f6dd85ee3010c2d81
HelloNuri <HelloNuri.dvl@gmail.com>
2018-12-02 22:39:09 +0900

    final

--- js/exit.js
+++ js/exit.js
@@ -1,8 +1,7 @@
-$('.effect.img-circle').click(function() {
-    console.log("1");
-    $('#namuwiki').fadeOut(1, function() {
-      console.log("2");
-    });
-  }
-
-);
+$(".effect.img-circle").click(function() {
+  $(".effect").animate({
+    opacity: 0.0
+  }, 2000, function() {
+    $(".effect").remove();
+  });
+});
